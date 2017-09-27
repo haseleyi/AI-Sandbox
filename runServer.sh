1f56fd19001574dbae3ad8c194361be7a7649e4d
echyam <echyam@gmail.com>
2017-09-27 21:04:52 +0200

    update for params of other search strats

--- runServer.sh
+++ runServer.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-if [ $2 ]
+if [ "$2" == "dfs" ]
 then
 	java -jar server.jar -l levels/$1 -c "java searchclient.SearchClient -dfs" -g 50 -t 300
-else
+elif [ "$2" == "bfs" ]
+then
 	java -jar server.jar -l levels/$1 -c "java searchclient.SearchClient" -g 50 -t 300
+elif [ "$2" == "astar" ]
+then
+	java -jar server.jar -l levels/$1 -c "java searchclient.SearchClient -astar" -g 50 -t 300
+elif [ "$2" == "wastar" ]
+then
+	java -jar server.jar -l levels/$1 -c "java searchclient.SearchClient -wastar" -g 50 -t 300
+elif [ "$2" == "greedy" ]
+then
+	java -jar server.jar -l levels/$1 -c "java searchclient.SearchClient -greedy" -g 50 -t 300
+else
+	echo "not a valid search strategy"
 fi
